f3817f24455e4d70c38f65a7071253662acf81c6
yll <yll@5201314.com>
2019-07-10 17:29:18 +0800

    首页静态页面

--- zxyj/src/App.vue
+++ zxyj/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <header>
-      <mt-search cancel-text="取消" placeholder="搜索"></mt-search>
+      <mt-search class="header-search" cancel-text="取消" placeholder="搜索"></mt-search>
     </header>
     <main>
       <router-view></router-view>
@@ -22,118 +22,127 @@
 </template>
 
 <script>
-  import Vue from "vue";
-  import MintUI from "mint-ui";
-  import "mint-ui/lib/style.css";
-  import App from "./App.vue";
-  import css from "./font/iconfont.css";
-  import rem from "./rem/rem.js";
+import Vue from "vue";
+import MintUI from "mint-ui";
+import "mint-ui/lib/style.css";
+import App from "./App.vue";
+import css from "./font/iconfont.css";
+import rem from "./rem/rem.js";
+Vue.use(MintUI);
 
-  Vue.use(MintUI);
-
-  export default {
-    name: "app",
-    data() {
-      return {
-        pages: [{
-            title: "首页",
-            path: "/home",
-            name: "Home",
-            font: "icon-shouye"
-          },
-          {
-            title: "分类",
-            path: "/classify",
-            name: "Classify",
-            font: "icon-ziyuan"
-          },
-          {
-            title: "购物车",
-            path: "/cart",
-            name: "Cart",
-            font: "icon-gouwuche"
-          },
-          {
-            title: "我的",
-            path: "/mine",
-            name: "Mine",
-            font: "icon-wode"
-          }
-        ]
-      };
-    },
-    components: {
-      App
-    }
-  };
+export default {
+  name: "app",
+  data() {
+    return {
+      pages: [
+        {
+          title: "首页",
+          path: "/home",
+          name: "Home",
+          font: "icon-shouye"
+        },
+        {
+          title: "分类",
+          path: "/classify",
+          name: "Classify",
+          font: "icon-ziyuan"
+        },
+        {
+          title: "购物车",
+          path: "/cart",
+          name: "Cart",
+          font: "icon-gouwuche"
+        },
+        {
+          title: "我的",
+          path: "/mine",
+          name: "Mine",
+          font: "icon-wode"
+        }
+      ]
+    };
+  },
+  components: {
+    App
+  }
+};
 </script>
 
 <style scope>
-  a {
-    text-decoration: none;
-  }
+a {
+  text-decoration: none;
+}
 
-  html,
-  body {
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
+html,
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 
-  ul li {
-    list-style: none;
-  }
+ul li {
+  list-style: none;
+}
 
-  #app {
-    height: 100%;
-    /* width: 100%; */
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-  }
+#app {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
 
-  /* 头部 */
-  header {
-    height: 1.066667rem;
-  }
+/* 头部 */
+header {
+  height: 1.066667rem;
+}
+header .header-search {
+  height: 100%;
+}
+/* 内容区 */
+main {
+  flex: 1;
+  height: 100%;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  width: 100%;
+}
 
-  /* 内容区 */
-  main {
-    flex: 1;
-  }
+/* 底部四件套 */
+footer {
+  height: 1.146667rem;
+}
 
-  /* 底部四件套 */
-  footer {
-    height: 1.146667rem;
-  }
+.item {
+  width: 100%;
+  display: flex;
+}
 
-  .item {
-    width: 100%;
-    display: flex;
-  }
+.item .item-btn {
+  height: 1.146667rem;
+  display: flex;
+  justify-content: center;
+  width: 25%;
+  padding-top: 0.133333rem;
+  box-sizing: border-box;
+}
 
-  .item .item-btn {
-    height: 1.146667rem;
-    display: flex;
-    justify-content: center;
-    width: 25%;
-    padding-top: 0.133333rem;
-    box-sizing: border-box;
-  }
+.item .item-btn .iconfont {
+  font-size: 0.506667rem;
+  color: #101010;
+}
 
-  .item .item-btn .iconfont {
-    font-size: 0.506667rem;
-    color: #101010;
-  }
+.item .item-btn .active {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+}
 
-  .item .item-btn .active {
-    width: 100%;
-    height: 100%;
-    text-align: center;
-  }
-
-  .item .item-btn .footer-item-font-size {
-    color: #7c7e86;
-    font-size: 0.24rem;
-  }
+.item .item-btn .footer-item-font-size {
+  color: #7c7e86;
+  font-size: 0.24rem;
+}
 </style>
--- zxyj/src/Foot/Home.vue
+++ zxyj/src/Foot/Home.vue
@@ -1,13 +1,371 @@
 <template>
   <div class="content">
-  
+    <!-- 轮播图 -->
+    <div class="header">
+      <mt-swipe :auto="4000">
+        <mt-swipe-item v-for="itemImg in banner">
+          <img :src="itemImg.urlImg" class="header-img" />
+        </mt-swipe-item>
+      </mt-swipe>
+    </div>
+    <!-- 四宫格 -->
+    <div class="fourGongGe">
+      <ul class="fourGongGe-item">
+        <li v-for="itemFont in fourGongGe" class="fourGongGe-item-list">
+          <img :src="itemFont.icon" />
+          <p>{{itemFont.label}}</p>
+        </li>
+      </ul>
+    </div>
+    <!-- 品牌特区 -->
+    <div class="brand">
+      <img :src="brand.img" alt />
+    </div>
+    <!-- 抢购专区 -->
+    <div class="buying" v-bind:style="{backgroundImage:'url(' + buying.image + ')'}">
+      <div class="buying-left"></div>
+      <div class="buying-right"></div>
+    </div>
+    <!-- 冰点价爆款 -->
+    <div class="hotbuy">
+      <div class="hotbuy-banner" v-bind:style="{backgroundImage:'url(' + hotbuy.banner + ')'}"></div>
+      <div class="hotbuy-item" v-bind:style="{backgroundImage:'url(' + hotbuy.image + ')'}">
+        <div class="hotbuy-item-left"></div>
+        <div class="hotbuy-item-right"></div>
+      </div>
+    </div>
+    <!-- 爆品推荐 -->
+    <div class="hotbrand">
+      <div class="hotbrand-banner" v-bind:style="{backgroundImage:'url(' + hotbrand.banner + ')'}"></div>
+      <div class="hotbrand-item" v-bind:style="{backgroundImage:'url(' + hotbrand.image + ')'}">
+        <div class="hotbrand-item-list hotbrand-item-top-left"></div>
+        <div class="hotbrand-item-list hotbrand-item-top-right"></div>
+        <div class="hotbrand-item-list hotbrand-item-bottom-left"></div>
+        <div class="hotbrand-item-list hotbrand-item-bottom-right"></div>
+      </div>
+    </div>
+    <!-- 商品秒杀 -->
+    <div class="miaosha">
+      <div class="miaosha-time"></div>
+      <div class="miaosha-goods">
+        <ul class="miaosha-goods-item">
+          <li class="miaosha-goods-item-list"></li>
+        </ul>
+      </div>
+    </div>
+    <!-- 品牌专区 -->
+    <div class="goodlist" v-for="goodsimgs in goodsimg" :key="goodsimgs.image">
+      <div class="goodlist-img" v-bind:style="{backgroundImage:'url(' + goodsimgs.image + ')'}"></div>
+      <div class="goodlist-item">
+        <div class="goodlist-top">
+          <p class="tuijian">热卖推荐</p>
+          <p class="gengduo">更多</p>
+        </div>
+        <div class="goodlist-flex">
+          <div class="goodlist-shop" v-for="goodslist in 20">
+            <div>
+              <img
+                style="width:1.333333rem "
+                src="http://img.zxyjsc.com/G1/M00/0C/84/rBLh9lrq7RSARc1dAASHZKcTK4Q491.jpg"
+              />
+            </div>
+            <div>
+              <p class="title">蚂蚁肉书友敬意等等</p>
+              <p class="price">￥39.00</p>
+              <p class="old-price">
+                <del>￥239.00</del>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import Vue from "vue";
+// 轮播图插件
+import { Swipe, SwipeItem } from "mint-ui";
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
 export default {
- 
+  data() {
+    return {
+      banner: [{ urlImg: "" }, { urlImg: "" }, { urlImg: "" }, { urlImg: "" }],
+      fourGongGe: [
+        {
+          label: "",
+          icon: ""
+        },
+        {
+          label: "",
+          icon: ""
+        },
+        {
+          label: "",
+          icon: ""
+        },
+        {
+          label: "",
+          icon: ""
+        }
+      ],
+      brand: {
+        img: ""
+      },
+      buying: [
+        {
+          img: ""
+        }
+      ],
+      hotbuy: [{ banner: "", image: "" }],
+      hotbrand: [{ banner: "", image: "" }],
+      goodsimg: [],
+      goodslist: []
+    };
+  },
+  async created() {
+    // 发起请求
+    let { data } = await this.$axios(
+      "https://api.zxyjsc.com/flyapi/pageConfig/getIndexConfig?pageId=home&version=1.0&terminal=3"
+    );
+
+    // 轮播图遍历
+    for (var i = 0; i < data.data.length; i++) {
+      if (data.data[i].type == "carousel") {
+        var bannerItem = data.data[i].data;
+        // console.log(bannerItem);
+        for (var j = 0; j < bannerItem.length; j++) {
+          this.banner[j].urlImg = bannerItem[j].image;
+        }
+      }
+    }
+
+    // 四宫格
+    for (var i = 0; i < data.data.length; i++) {
+      if (data.data[i].type == "links") {
+        var bannerItem = data.data[i].data;
+        for (var j = 0; j < bannerItem.length; j++) {
+          this.fourGongGe[j].label = bannerItem[j].label;
+          this.fourGongGe[j].icon = bannerItem[j].icon;
+        }
+      }
+    }
+
+    // 品牌img
+    this.brand.img = data.data[2].data.image;
+
+    // 抢购img
+    for (var i = 0; i < data.data[3].data.length; i++) {
+      this.buying.image = data.data[3].data[i].image;
+    }
+
+    // 冰点爆款
+    this.hotbuy.banner = data.data[4].data.image;
+    this.hotbuy.image = data.data[5].data[0].image;
+
+    // 爆款推荐
+    this.hotbrand.banner = data.data[6].data.image;
+    this.hotbrand.image = data.data[7].data[0].image;
+
+    // 商品秒杀
+
+    // 重复商品图
+    let goodimg = data.data.slice(9);
+    // console.log(goodimg);
+
+    for (var i = 0; i < goodimg.length; i++) {
+      if (goodimg[i].type == "banner") {
+        var bannerItem = goodimg[i].data;
+        this.goodsimg.push(bannerItem);
+      }
+    }
+
+    for (var i = 0; i < goodimg.length; i++) {
+      if (goodimg[i].title == "热卖推荐") {
+        // var bannerItem = goodimg[i];
+        // console.log(bannerItem);
+        let goodlistImg = await this.$axios.get(
+          "https://api.zxyjsc.com/flyapi/product/skulist?skuIds=" +
+            goodimg[i].data +
+            "&version=1.0&terminal=3"
+        );
+        console.log(goodlistImg);
+      }
+    }
+  },
+  methods: {}
 };
 </script>
 <style scoped>
+.content {
+  height: 100%;
+  width: 100%;
+}
+.header {
+  width: 100%;
+  height: 6.666667rem;
+}
 
+.header .header-img {
+  width: 100%;
+  height: 6.666667rem;
+  width: 100%;
+}
+.fourGongGe {
+  font-size: 0.293333rem;
+  color: #da053b;
+  line-height: 0.293333rem;
+}
+
+.fourGongGe .fourGongGe-item {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 2.32rem;
+}
+
+.fourGongGe .fourGongGe-item-list {
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.fourGongGe .fourGongGe-item-list img {
+  width: 1.093333rem;
+}
+.fourGongGe .fourGongGe-item-list p {
+  margin-top: 0.12rem;
+}
+.brand {
+  width: 100%;
+  height: 3.413333rem;
+  margin-bottom: 0.066667rem;
+}
+.brand img {
+  width: 100%;
+}
+.buying {
+  background-size: 100%;
+  background-repeat: no-repeat;
+  height: 3.066667rem;
+  width: 100%;
+  display: inline-flex;
+}
+.buying .buying-left {
+  height: 100%;
+  width: 50%;
+}
+.buying .buying-right {
+  height: 100%;
+  width: 50%;
+}
+.hotbuy {
+  height: 4.506667rem;
+}
+.hotbuy .hotbuy-banner {
+  background-size: 100%;
+  height: 1.2rem;
+}
+.hotbuy .hotbuy-item {
+  background-size: 100%;
+  background-repeat: no-repeat;
+  margin-top: 0.266667rem;
+  height: 3.573333rem;
+  width: 100%;
+
+  display: inline-flex;
+}
+.hotbuy .hotbuy-item-left {
+  height: 100%;
+  width: 50%;
+}
+.hotbuy .hotbuy-item-right {
+  height: 100%;
+  width: 50%;
+}
+.hotbrand .hotbrand-banner {
+  margin-top: 0.56rem;
+  background-size: 100%;
+  height: 1.2rem;
+  background-repeat: no-repeat;
+}
+.hotbrand .hotbrand-item {
+  height: 8.64rem;
+  background-size: 100%;
+  background-repeat: no-repeat;
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+}
+.hotbrand .hotbrand-item .hotbrand-item-list {
+  height: 50%;
+  width: 50%;
+}
+.miaosha {
+  height: 9.266667rem;
+}
+.goodlist .goodlist-img {
+  width: 100%;
+  height: 3.866667rem;
+  background-size: 100%;
+  background-repeat: no-repeat;
+}
+.goodlist .goodlist-top {
+  margin-top: .293333rem;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 0.293333rem 0 0.373333rem;
+  margin-bottom: .56rem
+}
+.goodlist .goodlist-top .tuijian{
+  font-size: .373333rem;
+}
+.goodlist .goodlist-top .gengduo{
+  color: #928f8d;
+  font-size: .293333rem;
+}
+.goodlist .goodlist-top .gengduo::after{
+  content: ">";
+  font-size: .4rem;
+  font-weight: 600;
+  line-height: .4rem
+}
+
+.goodlist .goodlist-flex {
+  box-sizing: border-box;
+  padding-left: 0.133333rem;
+  display: flex;
+  width: 100%;
+  overflow-x: auto;
+  flex-wrap: nowrap;
+}
+.goodlist .goodlist-flex .goodlist-shop {
+  width: 2.293333rem;
+  margin-right: 0.386667rem;
+  text-align: center;
+}
+.goodlist .goodlist-flex .goodlist-shop .title {
+  line-height: 0.293333rem;
+  font-size: 0.293333rem;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 100%;
+  overflow: hidden;
+  margin-bottom: 0.506667rem;
+}
+.goodlist .goodlist-flex .goodlist-shop .price {
+  line-height: 0.293333rem;
+  font-size: 0.293333rem;
+  color: #f40046;
+  margin-bottom: 12px;
+}
+.goodlist .goodlist-flex .goodlist-shop .old-price {
+  color: #7e8691;
+  font-size: 0.213333rem;
+  line-height: 0.213333rem;
+}
 </style>
